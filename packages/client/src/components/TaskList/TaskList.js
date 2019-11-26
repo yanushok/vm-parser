0ce74e05890f16d2e39de675178a005c32514079
Yan Yunitski <mr_younger@mail.ru>
2019-11-26 12:31:10 +0300

    Refactoring

--- packages/client/src/components/TaskList/TaskList.js
+++ packages/client/src/components/TaskList/TaskList.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Button from "components/Button";
 import { useGlobalState } from "contexts/state";
 import TasksApi from "services/tasksApi";
 import { deleteAction } from "actions/taskActions";
 
-function TaskList() {
+function TaskList({ onTaskClick }) {
     const { state, dispatch } = useGlobalState();
     
-    const onDelete = id => e => {
+    const deleteHandler = id => e => {
         TasksApi
             .deleteTask(id)
             .then(deleteAction)
             .then(dispatch);
     }
+
+    const taskClickHandler = useCallback(e => {
+        e.preventDefault();
+        onTaskClick();
+    }, [onTaskClick])
 
     if (!state || !state.data || !state.data.length) {
         return <h3>There are no tasks yet</h3>;
@@ -29,10 +34,12 @@ function TaskList() {
             <tbody>
                 {state.data.map(task => (
                     <tr key={task.id}>
-                        <td>{task.id}</td>
+                        <td>
+                            <a href="/" onClick={taskClickHandler}>{task.id}</a>
+                        </td>
                         <td>{task.status}</td>
                         <td>
-                            <Button onClick={onDelete(task.id)}>Delete</Button>
+                            <Button onClick={deleteHandler(task.id)}>Delete</Button>
                         </td>
                     </tr>
                 ))}
